3e8597967e75465d28b1b5683cf0ec99198e5892
thanhtv <thanhtv@vitalify.asia>
2017-11-09 11:13:45 +0700

    Add some package.

--- app/client/components/Login/Login.jsx
+++ app/client/components/Login/Login.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Router, Route, Link, History } from 'react-router';
 
 class Login extends React.Component {
   render() {
     return (
-      <div style={{textAlign: 'center'}}>
+      <div className="Login">
         <h1>Login Page</h1>
+        <br />
+        <form>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              autoFocus
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block-center
+            bsSize="large"
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
       </div>
     );
   }
